918a07d345e63c66784be976b5ea540f9d3c9395
Andy Olshansky <olshansa@gmail.com>
2024-09-15 15:52:24 -0400

    Added transacrion types

--- index.ts
+++ index.ts
@@ -1,77 +1,187 @@
 import crypto from 'crypto';
 
-class Block {
-    index: number;
+class Transaction {
+    fromAddress: string | null;
+    toAddress: string;
+    amount: number;
     timestamp: number;
-    data: any;
+    signature: string | null;
+
+    constructor(fromAddress: string | null, toAddress: string, amount: number) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+        this.timestamp = Date.now();
+        this.signature = null;
+    }
+
+    calculateHash() {
+        return crypto.createHash('sha256').update(
+            this.fromAddress +
+            this.toAddress +
+            this.amount +
+            this.timestamp
+        ).digest('hex');
+    }
+
+    signTransaction(privateKey: string) {
+        if (!this.fromAddress) {
+            throw new Error('Cannot sign transactions for mining rewards');
+        }
+
+        const txHash = this.calculateHash();
+        const sign = crypto.createSign('SHA256');
+        sign.update(txHash);
+        this.signature = sign.sign(privateKey, 'hex');
+    }
+
+    isValid() {
+        if (this.fromAddress === null) return true;
+
+        if (!this.signature || this.signature.length === 0) {
+            throw new Error('No signature in this transaction');
+        }
+
+        const txHash = this.calculateHash();
+        const verify = crypto.createVerify('SHA256');
+        verify.update(txHash);
+        return verify.verify(this.fromAddress, this.signature, 'hex');
+    }
+}
+
+class Cube {
+    timestamp: number;
     previousHash: string;
+    transactions: Transaction[];
     hash: string;
     nonce: number;
 
     // nonce is the number of times hash has been calculated to ensure that the hash starts with the required number of zeros
     // used as proof of work
-    constructor(index: number, timestamp: number, data: any, previousHash = '') {
-        this.index = index;
+    constructor(timestamp: number, transactions: Transaction[], previousHash = '') {
         this.timestamp = timestamp;
-        this.data = data;
         this.previousHash = previousHash;
+        this.transactions = transactions;
         this.hash = this.calculateHash();
         this.nonce = 0;
+        
     }
 
     calculateHash() {
         return crypto.createHash('sha256').update(
-            this.index +
             this.previousHash +
             this.timestamp +
-            JSON.stringify(this.data) +
+            JSON.stringify(this.transactions) +
             this.nonce
         ).digest('hex');
     }
 
     // Difficulty is the number of leading zeros the hash should start with after encryption to increase computation time
-    mineBlock(difficulty: number) {
+    mineCube(difficulty: number) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        console.log('Block mined: ' + this.hash);
+        console.log('Cube mined: ' + this.hash);
+    }
+
+    hasValidTransactions() {
+        for (const tx of this.transactions) {
+            if (!tx.isValid()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
-class BlockChain {
-    chain: Block[];
+class CubeChain {
+    chain: Cube[];
     difficulty: number;
+    pendingTransactions: Transaction[];
+    miningReward: number;
 
-    constructor(difficulty = 2) {
-        this.chain = [this.createGenesisBlock()];
+    constructor(difficulty = 2, pendingTransactions = [], miningReward = 100) {
+        this.chain = [this.createGenesisCube()];
         this.difficulty = difficulty;
+        this.pendingTransactions = pendingTransactions;
+        this.miningReward = miningReward;
     }
 
-    createGenesisBlock() {
-        return new Block(0, Date.now(), "Genesis Block", "0");
+    createGenesisCube() {
+        return new Cube(Date.now(), [], "0");
     }
 
-    getLatestBlock() {
+    getLatestCube() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock: Block) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress: string) {
+        const txReward = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(txReward);
+
+        let cube = new Cube(Date.now(), this.pendingTransactions, this.getLatestCube().hash);
+        cube.mineCube(this.difficulty);
+        this.chain.push(cube);
+
+        this.pendingTransactions = [];
+    }
+    
+    addTransaction(transaction: Transaction) {
+        if (!transaction.fromAddress || !transaction.toAddress) {
+            throw new Error('Transaction must include from and to address');
+        }
+
+        if (!transaction.isValid()) {
+            throw new Error('Cannot add invalid transaction to chain');
+        }
+
+        if (transaction.amount <= 0) {
+            throw new Error('Transaction amount should be higher than 0');
+        }
+
+        if (transaction.fromAddress !== null) {
+            const walletBalance = this.getBalanceOfAddress(transaction.fromAddress);
+            if (walletBalance < transaction.amount) {
+                throw new Error('Not enough balance');
+            }
+        }
+
+        this.pendingTransactions.push(transaction);
     }
 
+    getBalanceOfAddress(address: string) {
+        let balance = 0;
+
+        for (const cube of this.chain) {
+            for (const tx of cube.transactions) {
+                if (tx.fromAddress === address) {
+                    balance -= tx.amount;
+                }
+
+                if (tx.toAddress === address) {
+                    balance += tx.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+        
     isChainValid() {
         for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
+            const currentCube = this.chain[i];
+            const previousCube = this.chain[i - 1];
 
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
+            if (!currentCube.hasValidTransactions()) {
                 return false;
             }
 
-            if (currentBlock.previousHash !== previousBlock.hash) {
+            if (currentCube.hash !== currentCube.calculateHash()) {
+                return false;
+            }
+
+            if (currentCube.previousHash !== previousCube.hash) {
                 return false;
             }
         }
@@ -80,20 +190,43 @@ class BlockChain {
     } 
 }
 
-// Testing the blockchain
-let coin = new BlockChain();
-console.log('Mining block 1...');
-coin.addBlock(new Block(1, Date.now(), { amount: 6 }));
-console.log('Mining block 2...');
-coin.addBlock(new Block(2, Date.now(), { amount: 12 }));
-console.log('Mining block 3...');
-coin.addBlock(new Block(3, Date.now(), { amount: 15 }));
-console.log('Mining block 4...');
-coin.addBlock(new Block(4, Date.now(), { amount: 2 }));
+// Testing the Cubechain
+let coin = new CubeChain();
 
-console.log(JSON.stringify(coin, null, 1));
-console.log('Is blockchain valid? ' + coin.isChainValid());
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: { type: 'spki', format: 'pem' },
+    privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+});
 
-// Tampering the blockchain
-coin.chain[1].data = { amount: 100 };
-console.log('Is blockchain valid? ' + coin.isChainValid());
+console.log('Mining initial cube...');
+coin.minePendingTransactions(publicKey);
+
+console.log('Mining second cube...');
+coin.minePendingTransactions(publicKey);
+
+console.log('Balance of miner is', coin.getBalanceOfAddress(publicKey));
+
+const tx1 = new Transaction(publicKey, 'address2', 50);
+tx1.signTransaction(privateKey);
+coin.addTransaction(tx1);
+
+console.log('Starting the miner...');
+coin.minePendingTransactions('minerAddress');
+
+console.log('Balance of sender is', coin.getBalanceOfAddress(publicKey));
+console.log('Balance of recipient is', coin.getBalanceOfAddress('address2'));
+console.log('Balance of miner is', coin.getBalanceOfAddress('minerAddress'));
+
+coin.minePendingTransactions('minerAddress');
+
+console.log('Balance of sender is', coin.getBalanceOfAddress(publicKey));
+console.log('Balance of recipient is', coin.getBalanceOfAddress('address2'));
+console.log('Balance of miner is', coin.getBalanceOfAddress('minerAddress'));
+
+// console.log(JSON.stringify(coin, null, 1));
+// console.log('Is Cubechain valid? ' + coin.isChainValid());
+
+// // Tampering the Cubechain
+// coin.chain[1].hash = "{ amount: 100 }";
+// console.log('Is Cubechain valid? ' + coin.isChainValid());
